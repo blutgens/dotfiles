5a2b558bc29c61b6ffcbd6647c3a9d9f9120eea1
blutgens <blutgens+gh@gmail.com>
2014-04-15 10:22:04 -0500

    added a lockfile to keep from having multiple iterations of grive running and piling up

--- bin/run-grive.sh
+++ bin/run-grive.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-if [ -d ~/Grive ] ; then
+
+if [ -f /tmp/$LOGNAME.lck ] ; then 
+    echo "grive still running" 
+    exit 1
+elif [ -d ~/Grive ] ; then
+    touch /tmp/$LOGNAME-grive.lck
     cd ~/Grive
     grive -l ~/.grive-sync.log
+    rm /tmp/$LOGNAME-grive.lck
 else 
     echo "There's no ~/Grive directory you moron."
 fi
